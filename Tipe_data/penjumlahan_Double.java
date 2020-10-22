147a63f0e75586f70fd7c85c1e7f6a3dba237f28
yuansheva <70207812+yuansheva@users.noreply.github.com>
2020-10-22 19:38:49 +0700

    Create penjumlahan_Double.java

---
+++ Tipe_data/penjumlahan_Double.java
@@ -0,0 +1,16 @@
+//Buatlah program untuk menampilkan hasil perhitungan berikut, dengan menggunakan perintah input sebuah angka koma
+
+package tipe_data;
+
+public class Github_td {
+	public static void main(String[] args) {
+		double a,b,c;
+		
+		a = 5.5;
+		b = 3.1;
+		c = a-b;
+		
+		System.out.println(c);
+	}
+
+}
